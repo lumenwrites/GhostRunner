b5c9b1c185efe9c35608e837e55798a378d7828d
Ray Alez <raymestalez@gmail.com>
2020-12-19 11:39:10 +0000

    Update

--- Environment/Coin.gd
+++ Environment/Coin.gd
@@ -9,7 +9,8 @@ func _on_Coin_body_entered(body):
 
 func move_coin():
 	var margin = 64+16
-	var pos_x = rand_range(margin, OS.get_window_safe_area().size.x - margin)
-	var pos_y = rand_range(margin, OS.get_window_safe_area().size.y - margin)
+	var pos_x = rand_range(margin, get_viewport_rect().size.x - margin)
+	var pos_y = rand_range(margin, get_viewport_rect().size.y - margin)
+	print(get_viewport_rect().size)
 	var random_pos = Vector2(pos_x,pos_y)
 	global_position = random_pos
